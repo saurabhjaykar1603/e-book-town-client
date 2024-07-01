28fc7f8713fe41508a6dfe0f4ed8b3f755d78d4e
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-07-01 20:38:02 +0530

    feat : add navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,19 +3,30 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav>
-      <div>
-        <Link href={"/"}>
-          <div className=" flex items-center gap-1">
-            <div className="relative">
-              <Hexagon />
-              <BookIcon />
+    <nav className=" border-b">
+      <div className="max-width-7xl mx-auto flex items-center justify-between py-4">
+        <div>
+          <Link href={"/"}>
+            <div className=" flex items-center gap-1">
+              <div className="relative">
+                <Hexagon />
+                <BookIcon />
+              </div>
+              <span className="text-xl font-bold uppercase tracking-tight text-primary-500">
+                E Book Town
+              </span>
             </div>
-            <span className="text-xl font-bold uppercase tracking-tight text-primary-500">E Book Town</span>
-          </div>
-        </Link>
+          </Link>
+        </div>
+        <div className="flex items-center gap-4 ">
+          <button className="h-10 rounded-md border border-primary-500 px-4 py-2 text-sm font-medium text-primary-500 transition-all hover:border-primary-100 hover:bg-primary-100 active:border-primary-200 active:bg-primary-200">
+            Sign in
+          </button>
+          <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700">
+            Sign up
+          </button>
+        </div>
       </div>
-      <div>right</div>
     </nav>
   );
 }
