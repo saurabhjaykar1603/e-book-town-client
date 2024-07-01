c16488d71fd2a6835c0fa19ecaea7980028a7f50
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-07-01 20:51:22 +0530

    feat : add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-   <h1>Welcome</h1>
-   </>
+    <>
+      <h1>Welcome</h1>
+      <Banner />
+    </>
   );
 }
