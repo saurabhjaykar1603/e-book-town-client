d390892bc9039ed3e874726f021b2e92768d15da
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-09-08 18:16:53 +0530

    "Updated DownloadButton text to add a period at the end."

--- src/app/book/[bookId]/components/DownloadButton.tsx
+++ src/app/book/[bookId]/components/DownloadButton.tsx
@@ -10,7 +10,7 @@ const DownloadButton = ({ fileLink }: { fileLink: string }) => {
         <button
             onClick={handleDownload}
             className="mt-10 h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-primary-600 active:bg-primary-700">
-            Download the book
+            Download the book .
         </button>
     );
 };
